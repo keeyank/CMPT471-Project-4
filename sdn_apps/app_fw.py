67e46d79898d4b58aad626304541ff65043121c2
Phan Van <tnhvn@Phans-MacBook-Air.local>
2022-11-16 23:21:59 -0800

    added from_json functions in app_fw.py and app_te.py

--- sdn_apps/app_fw.py
+++ sdn_apps/app_fw.py
@@ -4,29 +4,42 @@ from app import NetworkApp
 from rule import Action, ActionType, Rule, MatchPattern
 from utils_json import DefaultEncoder
 
+
 def parse_action(d):
     if 'action_type' in d:
         return {
             'action_type': getattr(ActionType, d['action_type']),
             'out_port': d.get('out_port', None)
-            }
+        }
     return d
 
 
 class FirewallApp(NetworkApp):
     def __init__(self, json_file, of_controller=None, priority=3):
-        super(FirewallApp, self).__init__(None, json_file, of_controller, priority)
+        super(FirewallApp, self).__init__(
+            None, json_file, of_controller, priority)
 
     # Translates the firewall policy file in `self.json_file` to a list of Rule objects `self.rules`
     def from_json(self):
-        with open('%s'% self.json_file) as f:
+        with open('%s' % self.json_file) as f:
             rules = json.load(f, object_hook=parse_action)
             # TODO: complete
+            self.rules = []
+            for rule in rules:
+                match_pattern = rule["match_pattern"]
+                action = rule["action"]
+                self.rules.append(Rule(switch_id=rule["switch_id"],
+                                       match_pattern=MatchPattern(match_pattern["src_mac"], match_pattern["dst_mac"], match_pattern["mac_proto"],
+                                                                  match_pattern["ip_proto"], match_pattern[
+                                                                      "src_ip"], match_pattern["dst_ip"],
+                                                                  match_pattern["src_port"], match_pattern["dst_port"], match_pattern["in-port"]),
+                                       action=Action(action["action_type"], action["out_port"])))
 
     # Writes the firewall policy to a JSON file
     def to_json(self, json_file):
-        with open('%s'% json_file, 'w', encoding='utf-8') as f:
-            json.dump(self.rules, f, ensure_ascii=False, indent=4, cls=DefaultEncoder)
+        with open('%s' % json_file, 'w', encoding='utf-8') as f:
+            json.dump(self.rules, f, ensure_ascii=False,
+                      indent=4, cls=DefaultEncoder)
 
     # This function calls `send_openflow_rules` only.
     # This is because the policy is the actual OpenFlow rules to be sent.
--- sdn_apps/app_te.py
+++ sdn_apps/app_te.py
@@ -7,13 +7,15 @@ from rule import MatchPattern
 from te_objs import PassByPathObjective, MinLatencyObjective, MaxBandwidthObjective
 from utils_json import DefaultEncoder
 
+
 class TEApp(NetworkApp):
     def __init__(self, topo_file, json_file, of_controller=None, priority=2):
-        super(TEApp, self).__init__(topo_file, json_file, of_controller, priority)
-        self.pass_by_paths_obj = [] # a list of PassByPathObjective objects 
-        self.min_latency_obj = [] # a list of MinLatencyObjective objects
-        self.max_bandwidth_obj = [] # a list of MaxBandwidthObjective objects
-    
+        super(TEApp, self).__init__(
+            topo_file, json_file, of_controller, priority)
+        self.pass_by_paths_obj = []  # a list of PassByPathObjective objects
+        self.min_latency_obj = []  # a list of MinLatencyObjective objects
+        self.max_bandwidth_obj = []  # a list of MaxBandwidthObjective objects
+
     def add_pass_by_path_obj(self, pass_by_obj):
         self.pass_by_paths_obj.append(pass_by_obj)
 
@@ -29,10 +31,46 @@ class TEApp(NetworkApp):
     #       self.min_latency_obj
     #       self.max_bandwidth_obj
     def from_json(self):
-        with open('%s'% self.json_file) as f:
+        with open('%s' % self.json_file) as f:
             # TODO: complete
+            objects = json.load(f)
+
+            pass_by_path_objs = objects["pass_by_path"]
+            for obj in pass_by_path_objs:
+                match_pattern = obj["match_pattern"]
+                pass_by_obj = PassByPathObjective(
+                    match_pattern=MatchPattern(match_pattern["src_mac"], match_pattern["dst_mac"], match_pattern["mac_proto"],
+                                               match_pattern["ip_proto"], match_pattern["src_ip"], match_pattern["dst_ip"],
+                                               match_pattern["src_port"], match_pattern["dst_port"], match_pattern["in-port"]),
+                    switches=obj["switches"],
+                    symmetric=obj["symmertric"])
+                self.add_pass_by_path_obj(pass_by_obj)
+
+            min_lat_objs = objects["min_latency"]
+            for obj in min_lat_objs:
+                match_pattern = obj["match_pattern"]
+                min_lat_obj = MinLatencyObjective(
+                    match_pattern=MatchPattern(match_pattern["src_mac"], match_pattern["dst_mac"], match_pattern["mac_proto"],
+                                               match_pattern["ip_proto"], match_pattern["src_ip"], match_pattern["dst_ip"],
+                                               match_pattern["src_port"], match_pattern["dst_port"], match_pattern["in-port"]),
+                    src_switch=obj["src_switch"],
+                    dst_switch=["st_switch"],
+                    symmetric=["symmetric"])
+                self.add_min_latency_obj(min_lat_obj)
+
+            max_bw_objs = objects["max_bandwidth"]
+            for obj in max_bw_objs:
+                match_pattern = obj["match_pattern"]
+                max_bw_obj = MaxBandwidthObjective(
+                    match_pattern=MatchPattern(match_pattern["src_mac"], match_pattern["dst_mac"], match_pattern["mac_proto"],
+                                               match_pattern["ip_proto"], match_pattern["src_ip"], match_pattern["dst_ip"],
+                                               match_pattern["src_port"], match_pattern["dst_port"], match_pattern["in-port"]),
+                    src_switch=obj["src_switch"],
+                    dst_switch=["st_switch"],
+                    symmetric=["symmetric"])
+                self.add_max_bandwidth_obj(max_bw_obj)
             pass
-    
+
     # Translates the TE objectives to the `json_file`
     def to_json(self, json_file):
         json_dict = {
@@ -41,38 +79,39 @@ class TEApp(NetworkApp):
             'max_bandwidth': self.max_bandwidth_obj,
         }
 
-        with open('%s'% json_file, 'w', encoding='utf-8') as f:
-            json.dump(json_dict, f, ensure_ascii=False, indent=4, cls=DefaultEncoder)
+        with open('%s' % json_file, 'w', encoding='utf-8') as f:
+            json.dump(json_dict, f, ensure_ascii=False,
+                      indent=4, cls=DefaultEncoder)
 
     # This function translates the objectives in `self.pass_by_paths_obj` to a list of Rules in `self.rules`
-    # It should: 
+    # It should:
     #   call `self.calculate_rules_for_path` as needed
-    #   handle traffic in reverse direction when `symmetric` is True 
+    #   handle traffic in reverse direction when `symmetric` is True
     #   call `self.send_openflow_rules()` at the end
     def provision_pass_by_paths(self):
         self.rules = []
         # TODO: complete
 
     # This function translates the objectives in `self.min_latency_obj` to a list of Rules in `self.rules`
-    # It should: 
+    # It should:
     #   call `self.calculate_rules_for_path` as needed
     #   consider using the function `networkx.shortest_path` in the networkx package
-    #   handle traffic in reverse direction when `symmetric` is True 
+    #   handle traffic in reverse direction when `symmetric` is True
     #   call `self.send_openflow_rules()` at the end
     def provision_min_latency_paths(self):
         self.rules = []
         # TODO: complete
 
-    # BONUS: 
+    # BONUS:
     # This function translates the objectives in `self.max_bandwidth_obj` to a list of Rules in `self.rules`
-    # It should: 
+    # It should:
     #   call `self.calculate_rules_for_path` as needed
     #   consider what algorithms to use (from networkx) to calculate the paths
-    #   handle traffic in reverse direction when `symmetric` is True 
+    #   handle traffic in reverse direction when `symmetric` is True
     #   call `self.send_openflow_rules()` at the end
     def provision_max_bandwidth_paths(self):
         pass
-    
+
     # BONUS: Used to react to changes in the network (the controller notifies the App)
     def on_notified(self, **kwargs):
         pass
